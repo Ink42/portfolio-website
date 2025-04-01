b45c75edbdc635d7c300fe85691e0ad881aa5141
lindomash001 <lindomash001@gmail.com>
2025-04-01 04:13:24 +0200

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
-
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH  || "/portfolio-website"
-})
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: '/portfolio-website/' 
+}));
